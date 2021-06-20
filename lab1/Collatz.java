05d3b1231d3cc2d0717a42732be530991c5698fb
hxlaf <xuhan@lafayette.edu>
2021-06-20 18:44:16 -0500

    Finished Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,18 @@
-/** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+/**
+ * Class that prints the Collatz sequence starting from a given number.
+ *
+ * @author YOUR NAME HERE
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * Method to compute the next number for Collatz seq
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
 
